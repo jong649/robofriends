4a9bf5351b433f9b4cd0b1560621cb95b25fcdd1
Jon <jong649@gmail.com>
2020-03-01 12:54:28 -0800

    Add redux & actions, constants, reducers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider, connect } from "react-redux";
+import { createStore } from "redux";
 import "./index.css";
 import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
+import { searchRobots } from "./reducers.js";
 import "tachyons";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(searchRobots);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
